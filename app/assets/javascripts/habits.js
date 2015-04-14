213f3da128ff44ec6b3f712551ed3dab2989b1f4
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-14 12:59:59 -0400

    Utilizing show/hide functionality, working, just need to get buttons to show as images.

--- app/assets/javascripts/habits.js
+++ app/assets/javascripts/habits.js
@@ -13,12 +13,39 @@ $(document).ready(function() {
       success: function(result) {
 
         action = '/users/' + result.user_id + '/dailyhabits/' + result.id;
-        var random = "#habit" + result.id
+        var complete_div = "#habit-" + result.id + "-complete"
+        var incomplete_div = "#habit-" + result.id + "-incomplete"
 
-            $(random).replaceWith('<div class="complete-task"> COMPLETE! </div>');
+            // $(random).replaceWith('<div class="complete-task"> COMPLETE! </div>');
+            $(complete_div).toggle();
+            $(incomplete_div).toggle();
 
             // $(random).replaceWith('<div class="complete-task"> COMPLETE! </div>');
         // $("#habitlist2").load('/users/1.html #habitlist2');
+      }
+    });
+   });
+});
+
+
+$(document).ready(function() {
+  $('[data-habit-button="incomplete"]').on('submit', function(event) {
+    event.preventDefault();
+     $form = $(event.currentTarget);
+     $.ajax({
+      type: "PATCH",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+        },
+      url: $form.attr('action'),
+      dataType: "json",
+      success: function(result) {
+
+        action = '/users/' + result.user_id + '/dailyhabits/' + result.id;
+        var complete_div = "#habit-" + result.id + "-complete"
+        var incomplete_div = "#habit-" + result.id + "-incomplete"
+            $(incomplete_div).toggle();
+            $(complete_div).toggle();
       }
     });
    });
