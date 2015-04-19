865592ce08edc06841cb326cde93f4eeea2a69dd
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-19 15:12:25 -0400

    Setup for heroku.

--- db/migrate/20150415183339_add_streak_count_to_dailyhabits.rb
+++ db/migrate/20150415183339_add_streak_count_to_dailyhabits.rb
@@ -1,5 +1,9 @@
 class AddStreakCountToDailyhabits < ActiveRecord::Migration
-  def change
+  def up
+    add_column :dailyhabits, :streak_count, :integer
+  end
+
+  def down
     remove_column :dailyhabits, :streak_count, :integer
   end
 end
