485405d573a496cab34bed54d45e203cd063037b
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-26 16:22:31 -0400

    rubocop updates.

--- spec/features/user_interacts_with_habits_spec.rb
+++ spec/features/user_interacts_with_habits_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
-
-feature 'User can sign up for plans on plans page', %Q{
+feature 'User can sign up for plans on plans page', %({
 As a user
 I should be able to sign up for habit/goal plans
 To help me achieve my goals
-} do
+}) do
+  describe 'user views plans' do
+    let(:user) { FactoryGirl.create(:user) }
 
-describe "user views plans" do
-
-  let(:user) { FactoryGirl.create(:user) }
-
-    scenario "I want to mark a habit as complete" do
-    sign_in_as(user)
-    plan = FactoryGirl.create(:plan)
-    habit = FactoryGirl.create(:habit)
-    dailyhabit = Dailyhabit.create(user_id: user.id, habit_id: habit.id, point_value: 0, plan_id: plan.id, date: DateTime.now)
-    visit plans_path
-    click_button("plan-#{plan.id}")
-    click_button("#{dailyhabit.id}-complete")
-    visit(current_path)
-    expect dailyhabit.completed_at != nil
+    scenario 'I want to mark a habit as complete' do
+      sign_in_as(user)
+      plan = FactoryGirl.create(:plan)
+      habit = FactoryGirl.create(:habit)
+      dailyhabit = Dailyhabit.create(
+        user_id: user.id, habit_id: habit.id,
+        point_value: 0, plan_id: plan.id, date: DateTime.now
+      )
+      visit plans_path
+      click_button("plan-#{plan.id}")
+      click_button("#{dailyhabit.id}-complete")
+      visit(current_path)
+      expect !dailyhabit.completed_at.nil?
     end
 
-
-    scenario "I want to mark a habit as complete" do
-    sign_in_as(user)
-    plan = FactoryGirl.create(:plan)
-    habit = FactoryGirl.create(:habit)
-    dailyhabit = Dailyhabit.create(user_id: user.id, habit_id: habit.id, point_value: 0, plan_id: plan.id, date: DateTime.now)
-    visit plans_path
-    click_button("plan-#{plan.id}")
-    click_button("#{dailyhabit.id}-complete")
-    visit(current_path)
-    click_button("#{dailyhabit.id}-incomplete")
-    visit(current_path)
-    expect dailyhabit.completed_at == nil
+    scenario 'I want to mark a habit as complete' do
+      sign_in_as(user)
+      plan = FactoryGirl.create(:plan)
+      habit = FactoryGirl.create(:habit)
+      dailyhabit = Dailyhabit.create(
+        user_id: user.id, habit_id: habit.id,
+        point_value: 0, plan_id: plan.id, date: DateTime.now
+      )
+      visit plans_path
+      click_button("plan-#{plan.id}")
+      click_button("#{dailyhabit.id}-complete")
+      visit(current_path)
+      click_button("#{dailyhabit.id}-incomplete")
+      visit(current_path)
+      expect dailyhabit.completed_at.nil?
     end
-
-
   end
 end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,94 +1,93 @@
 require 'rails_helper'
-
-feature 'User signs up', %Q{
+feature 'User signs up', %({
 As a user
 I want to sign in
 So that my questions and answers can be associated to me.
-} do
-
+}) do
   scenario 'user provides valid information' do
     visit root_path
-    click_on "Sign Up"
+    click_on 'Sign Up'
 
-    fill_in "First Name", with: "Paul"
-    fill_in "Last Name", with: "Parmesan"
-    fill_in "Email", with: "pparmasean@privateeyes.com"
-    fill_in "Password", with: "supersecret"
-    fill_in "Password confirmation", with: "supersecret"
-    click_on "Sign up"
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparmasean@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'supersecret'
+    click_on 'Sign up'
 
-    expect(page).to have_content "Welcome! You have signed up successfully."
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
   scenario 'user passwords do not match' do
     visit root_path
-    click_on "Sign Up"
+    click_on 'Sign Up'
 
-    fill_in "First Name", with: "Paul"
-    fill_in "Last Name", with: "Parmesan"
-    fill_in "Email", with: "pparmasean@privateeyes.com"
-    fill_in "Password", with: "supersecret"
-    fill_in "Password confirmation", with: "doesnotmatch"
-    click_on "Sign up"
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparmasean@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'doesnotmatch'
+    click_on 'Sign up'
 
     expect(page).to have_content "Password confirmation doesn't match"
   end
 
   scenario 'user email is already registered' do
-    existing_user = User.create(first_name: "Paul", last_name: "Parmesan",
-    email: "pparmasean@privateeyes.com",
-    password: "supersecret"
+    existing_user = User.create(
+      first_name: 'Paul', last_name: 'Parmesan',
+      email: 'pparmasean@privateeyes.com',
+      password: 'supersecret'
     )
     visit root_path
-    click_on "Sign Up"
+    click_on 'Sign Up'
 
-    fill_in "First Name", with: existing_user.first_name
-    fill_in "Last Name", with: existing_user.last_name
-    fill_in "Email", with: existing_user.email
-    fill_in "Password", with: existing_user.password
-    fill_in "Password confirmation", with: existing_user.password
-    click_on "Sign up"
+    fill_in 'First Name', with: existing_user.first_name
+    fill_in 'Last Name', with: existing_user.last_name
+    fill_in 'Email', with: existing_user.email
+    fill_in 'Password', with: existing_user.password
+    fill_in 'Password confirmation', with: existing_user.password
+    click_on 'Sign up'
 
-    expect(page).to have_content "Email has already been taken"
+    expect(page).to have_content 'Email has already been taken'
   end
 
   scenario 'user email is formatted incorrectly' do
     visit root_path
-    click_on "Sign Up"
+    click_on 'Sign Up'
 
-    fill_in "First Name", with: "Paul"
-    fill_in "Last Name", with: "Parmesan"
-    fill_in "Email", with: "pparma"
-    fill_in "Password", with: "supersecret"
-    fill_in "Password confirmation", with: "doesnotmatch"
-    click_on "Sign up"
-    expect(page).to have_content "Email is invalid"
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparma'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'doesnotmatch'
+    click_on 'Sign up'
+    expect(page).to have_content 'Email is invalid'
   end
 
   scenario 'user is able to log out' do
     visit root_path
-    click_on "Sign Up"
+    click_on 'Sign Up'
 
-    fill_in "First Name", with: "Paul"
-    fill_in "Last Name", with: "Parmesan"
-    fill_in "Email", with: "pparmasean@privateeyes.com"
-    fill_in "Password", with: "supersecret"
-    fill_in "Password confirmation", with: "supersecret"
-    click_on "Sign up"
-
-    visit root_path
-    click_link('user_logout')
-
-    visit root_path
-    click_on "Sign In"
-
-    fill_in "Email", with: "pparmasean@privateeyes.com"
-    fill_in "Password", with: "supersecret"
-    click_on "Log in"
-    expect(page).to have_content "Signed in successfully."
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparmasean@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'supersecret'
+    click_on 'Sign up'
 
     visit root_path
     click_link('user_logout')
-    expect(page).to have_content "Signed out successfully."
+
+    visit root_path
+    click_on 'Sign In'
+
+    fill_in 'Email', with: 'pparmasean@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    click_on 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+
+    visit root_path
+    click_link('user_logout')
+    expect(page).to have_content 'Signed out successfully.'
   end
 end
