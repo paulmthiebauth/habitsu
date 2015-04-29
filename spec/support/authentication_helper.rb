b48e939071386f9058c9fb6aad40d6d643018693
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-29 00:04:20 -0400

    updating a few tests.

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -5,4 +5,37 @@ module AuthenticationHelper
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def user_sign_up
+    visit root_path
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparma@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'supersecret'
+    click_on 'Sign up'
+  end
+
+  def user_sign_up_invalid_password
+    visit root_path
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparma@privateeyes.com'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'super'
+    click_on 'Sign up'
+  end
+
+  def user_sign_up_invalid_email
+    visit root_path
+    click_on 'Sign Up'
+    fill_in 'First Name', with: 'Paul'
+    fill_in 'Last Name', with: 'Parmesan'
+    fill_in 'Email', with: 'pparmas'
+    fill_in 'Password', with: 'supersecret'
+    fill_in 'Password confirmation', with: 'supersecret'
+    click_on 'Sign up'
+  end
 end
